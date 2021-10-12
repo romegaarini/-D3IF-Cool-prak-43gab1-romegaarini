58a206962ece4a7d7e4cf82dd0370d7216be68f5
romegaarini <romega.purba123@gmail.com>
2021-10-12 16:25:05 +0700

    3.1.Membuat Menu untuk Tampilan Peta

--- app/src/main/java/org/d3if0031/covid19/MainActivity.kt
+++ app/src/main/java/org/d3if0031/covid19/MainActivity.kt
@@ -2,10 +2,24 @@ package org.d3if0031.covid19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_maps) {
+            Log.d("MAPS", "Menu maps diklik!")
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
